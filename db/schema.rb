ed539bd91893a92455f2fcd7314d7776244a528d
Liz Vanderkloot <liz.m.vdk@gmail.com>
2014-12-30 17:40:38 -0500

    Adds bio to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141230184300) do
+ActiveRecord::Schema.define(version: 20141230223735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,11 @@ ActiveRecord::Schema.define(version: 20141230184300) do
     t.string   "username",                            null: false
     t.string   "first_name"
     t.string   "last_initial"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
+
 end

6a88ec601ffb9c081cf5ed1b8a951cff4c53fc72
Liz Vanderkloot <liz.m.vdk@gmail.com>
2014-12-30 18:16:41 -0500

    Add link to edit profile onuser profile page

--- spec/features/view_a_users_profile_spec.rb
+++ spec/features/view_a_users_profile_spec.rb
@@ -12,18 +12,32 @@ feature "user profile", %{
     # * The profile shows the user's reviews
     # * The profile shows the user's comments
     # * If I view my own profile there is a link to edit it
+    # * A user may not edit a profile that is not their own
+
+  let(:user) { FactoryGirl.create(:user) }
 
   scenario "vist another user's profile" do
-    user = FactoryGirl.create(:user)
-
     visit user_path(user)
-    save_and_open_page
 
     expect(page).to have_content user.username
     expect(page).to have_content user.bio
     expect(page).to have_content user.first_name
     expect(page).to have_content user.last_initial
+    expect(page).to_not have_content "Edit Profile"
     # We will need to implement reviews and comments once the models
     # are implemented
+  end
+
+  scenario "user visits his/her own profile" do
+
+    sign_in user
+
+    visit user_path(user)
+
+    expect(page).to have_content user.username
+    expect(page).to have_content user.bio
+    expect(page).to have_content user.first_name
+    expect(page).to have_content user.last_initial
+    expect(page).to have_content "Edit Profile"
   end
 end

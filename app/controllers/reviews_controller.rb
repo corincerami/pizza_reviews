5c2aa6bc8fdab2da9c847b992e3eea4f65a92e70
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-01-01 17:41:10 -0500

    Fix tabs issue

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,55 +1,55 @@
 class ReviewsController < ApplicationController
-	before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
-	def show
-		@review = Review.find(params[:id])
-		@pizzeria = Pizzeria.find(@review.pizzeria_id)
-	end
+  def show
+    @review = Review.find(params[:id])
+    @pizzeria = Pizzeria.find(@review.pizzeria_id)
+  end
 
-	def new
-		@pizzeria = Pizzeria.find(params[:pizzeria_id])
-		@review = Review.new
-	end
+  def new
+    @pizzeria = Pizzeria.find(params[:pizzeria_id])
+    @review = Review.new
+  end
 
-	def create
-		@pizzeria = Pizzeria.find(params[:pizzeria_id])
-		@review = @pizzeria.reviews.build(review_params)
-		@review.user = current_user
-		if @review.save
-			flash[:notice] = "Review created"
-			redirect_to pizzeria_review_path(@pizzeria, @review)
-		else
-			render :new
-		end
-	end
+  def create
+    @pizzeria = Pizzeria.find(params[:pizzeria_id])
+    @review = @pizzeria.reviews.build(review_params)
+    @review.user = current_user
+    if @review.save
+      flash[:notice] = "Review created"
+      redirect_to pizzeria_review_path(@pizzeria, @review)
+    else
+      render :new
+    end
+  end
 
-	def edit
-		@review = Review.find(params[:id])
-		@pizzeria = Pizzeria.find(@review.pizzeria_id)
-		if !correct_user?
-			flash[:error] = "You do not have permission to edit this review"
-			render :show
-		end
-	end
+  def edit
+    @review = Review.find(params[:id])
+    @pizzeria = Pizzeria.find(@review.pizzeria_id)
+    if !correct_user?
+      flash[:error] = "You do not have permission to edit this review"
+      render :show
+    end
+  end
 
-	def update
-		@review = Review.find(params[:id])
-		@pizzeria = Pizzeria.find(@review.pizzeria_id)
-		if correct_user? && @review.update(review_params)
-			flash[:notice] = "Review Updated Sucessfully"
-			redirect_to pizzeria_review_path(@pizzeria, @review)
-		else
-			render :edit
-		end
-	end
-	
-	private
+  def update
+    @review = Review.find(params[:id])
+    @pizzeria = Pizzeria.find(@review.pizzeria_id)
+    if correct_user? && @review.update(review_params)
+      flash[:notice] = "Review Updated Sucessfully"
+      redirect_to pizzeria_review_path(@pizzeria, @review)
+    else
+      render :edit
+    end
+  end
 
-	def review_params
-		review_params = params.require(:review).permit(:title, :body, :rating)
-	end
+  private
 
-	def correct_user?
-		current_user == @review.user
-	end
+  def review_params
+    params.require(:review).permit(:title, :body, :rating)
+  end
+
+  def correct_user?
+    current_user == @review.user
+  end
 end

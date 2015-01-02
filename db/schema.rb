5ae2244edfade4e03321080765a2d9f16ea68587
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-01-02 14:12:39 -0500

    Rename comment column to body in comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141231212151) do
+ActiveRecord::Schema.define(version: 20150102190624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string   "title"
-    t.text     "comment",    null: false
+    t.text     "body",       null: false
     t.integer  "user_id",    null: false
     t.integer  "review_id",  null: false
     t.datetime "created_at"

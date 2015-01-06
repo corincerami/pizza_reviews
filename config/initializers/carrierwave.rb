596c05053763e761a7269988bf2bc5c787840be9
Chris C Cerami <chrisccerami@gmail.com>
2015-01-06 16:39:24 -0500

    Added default aws_keys as blank strings in carrierwave.rb config options

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV["PRODUCTION_AWS_ACCESS_KEY_ID"],
-    :aws_secret_access_key  => ENV["PRODUCTION_AWS_SECRET_KEY"]
+    :aws_access_key_id      => ENV["PRODUCTION_AWS_ACCESS_KEY_ID"] || "",
+    :aws_secret_access_key  => ENV["PRODUCTION_AWS_SECRET_KEY"] || ""
   }
-  config.fog_directory  = ENV["PRODUCTION_AWS_BUCKET"]                          # required
+  config.fog_directory  = ENV["PRODUCTION_AWS_BUCKET"] || ""
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
 end

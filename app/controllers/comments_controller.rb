9ebe049fde829334620fb6551b7ed3ffd50e7df5
Chris C Cerami <chrisccerami@gmail.com>
2015-01-06 13:24:29 -0500

    Users can now downvote reviews

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,9 @@ class CommentsController < ApplicationController
   def create
     @review = Review.find(params[:review_id])
     @comment = Comment.new(comment_params)
+    @vote = Vote.new
     @comment.user = current_user
     @comment.review = @review
-
     if @comment.save
       redirect_to review_path(@review),
                   notice: "Comment has been posted sucessfully"

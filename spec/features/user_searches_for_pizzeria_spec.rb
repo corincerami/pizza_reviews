0ea5200273cccdf55a4f04ab9b76d6dc13e2f3fa
Chris C Cerami <chrisccerami@gmail.com>
2015-01-08 11:52:43 -0500

    Added test for searching with 0 results

--- spec/features/user_searches_for_pizzeria_spec.rb
+++ spec/features/user_searches_for_pizzeria_spec.rb
@@ -1,19 +1,35 @@
 require "rails_helper"
 
 feature "Search pizzerias" do
+  before(:each) do
+    @regina = FactoryGirl.create(:pizzeria, name: "Pizzeria Regina")
+    @crazy_kitchen = FactoryGirl.create(:pizzeria, name: "Crazy Kitchen Pizza")
+    @crazy_dough = FactoryGirl.create(:pizzeria, name: "Crazy Dough")
+  end
+
   scenario "fill in search form and view results" do
-    regina = FactoryGirl.create(:pizzeria, name: "Pizzeria Regina")
-    crazy_kitchen = FactoryGirl.create(:pizzeria, name: "Crazy Kitchen Pizza")
-    crazy_dough = FactoryGirl.create(:pizzeria, name: "Crazy Dough")
 
     visit pizzerias_path
     fill_in "Search", with: "crazy"
     click_button "Search Pizzerias"
 
-    expect(page).to have_link crazy_kitchen.name,
-                              href: pizzeria_path(crazy_kitchen)
-    expect(page).to have_link crazy_dough.name,
-                              href: pizzeria_path(crazy_dough)
-    expect(page).not_to have_content regina.name
+    expect(page).to have_link @crazy_kitchen.name,
+                              href: pizzeria_path(@crazy_kitchen)
+    expect(page).to have_link @crazy_dough.name,
+                              href: pizzeria_path(@crazy_dough)
+    expect(page).not_to have_content @regina.name
+  end
+
+  scenario "fill in search form and find no matches" do
+    visit pizzerias_path
+    fill_in "Search", with: "nothing"
+    click_button "Search Pizzerias"
+
+    expect(page).to have_link @crazy_kitchen.name,
+                              href: pizzeria_path(@crazy_kitchen)
+    expect(page).to have_link @crazy_dough.name,
+                              href: pizzeria_path(@crazy_dough)
+    expect(page).to have_link @regina.name,
+                              href: pizzeria_path(@regina)
   end
 end

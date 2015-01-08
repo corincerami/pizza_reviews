d8163e44a8bca1de6d6a367cc856ff0e3087a7ba
Chris C Cerami <chrisccerami@gmail.com>
2015-01-08 14:00:17 -0500

    Reformatted seeds file to match Hound style guides

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,59 +1,19 @@
-pizzerias = Yelp.client.search("Boston", { term: "pizza", 
-                                           category_filter: "pizza",
-                                           sort: 2 })
+cities = ["Boston", "Somerville", "Jamaica Plain", "Cambridge"]
 
-pizzerias.businesses.each do |pizzeria|
-  pizza = Pizzeria.create(name: pizzeria.name, 
-                   street: pizzeria.location.address[0], 
-                   city: pizzeria.location.city, 
-                   state: pizzeria.location.state_code,
-                   zip_code: pizzeria.location.postal_code,
-                   phone: pizzeria.display_phone,
-                   website: pizzeria.url,
-                   photo: pizzeria.image_url)
-  puts "Pizzeria #{pizza.name} created"
-end
+cities.each do |city|
+  pizzerias = Yelp.client.search(city, term: "pizza",
+                                       category_filter: "pizza",
+                                       sort: 2)
 
-pizzerias = Yelp.client.search("Somerville", { term: "pizza", 
-                                           category_filter: "pizza",
-                                           sort: 2 })
-
-pizzerias.businesses.each do |pizzeria|
-  pizza = Pizzeria.create(name: pizzeria.name, 
-                   street: pizzeria.location.address[0], 
-                   city: pizzeria.location.city, 
-                   state: pizzeria.location.state_code,
-                   zip_code: pizzeria.location.postal_code,
-                   phone: pizzeria.display_phone,
-                   website: pizzeria.url,
-                   photo: pizzeria.image_url)
-  puts "Pizzeria #{pizza.name} created"
-end
-pizzerias = Yelp.client.search("Cambridge", { term: "pizza", 
-                                           category_filter: "pizza",
-                                           sort: 2 })
-pizzerias.businesses.each do |pizzeria|
-  pizza = Pizzeria.create(name: pizzeria.name, 
-                   street: pizzeria.location.address[0], 
-                   city: pizzeria.location.city, 
-                   state: pizzeria.location.state_code,
-                   zip_code: pizzeria.location.postal_code,
-                   phone: pizzeria.display_phone,
-                   website: pizzeria.url,
-                   photo: pizzeria.image_url)
-  puts "Pizzeria #{pizza.name} created"
-end
-pizzerias = Yelp.client.search("Jamaica Plain", { term: "pizza", 
-                                           category_filter: "pizza",
-                                           sort: 2 })
-pizzerias.businesses.each do |pizzeria|
-  pizza = Pizzeria.create(name: pizzeria.name, 
-                   street: pizzeria.location.address[0], 
-                   city: pizzeria.location.city, 
-                   state: pizzeria.location.state_code,
-                   zip_code: pizzeria.location.postal_code,
-                   phone: pizzeria.display_phone,
-                   website: pizzeria.url,
-                   photo: pizzeria.image_url)
-  puts "Pizzeria #{pizza.name} created"
+  pizzerias.businesses.each do |pizzeria|
+    pizza = Pizzeria.create(name: pizzeria.name,
+                            street: pizzeria.location.address[0],
+                            city: pizzeria.location.city,
+                            state: pizzeria.location.state_code,
+                            zip_code: pizzeria.location.postal_code,
+                            phone: pizzeria.display_phone,
+                            website: pizzeria.url,
+                            photo: pizzeria.image_url)
+    puts "Pizzeria #{pizza.name} created"
+  end
 end

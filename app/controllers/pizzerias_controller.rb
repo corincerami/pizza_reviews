0f3139fc3d8b847ae9d4f019a452ac0e9756a54b
henrykerron <henrykerron@gmail.com>
2015-01-09 14:48:55 -0500

    Add feature so only admins can edit pizzeria

--- app/controllers/pizzerias_controller.rb
+++ app/controllers/pizzerias_controller.rb
@@ -1,6 +1,6 @@
 class PizzeriasController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_filter :authorize!, only: [:destroy]
+  before_filter :authorize!, only: [:destroy, :edit, :update]
 
   def new
     @pizzeria = Pizzeria.new

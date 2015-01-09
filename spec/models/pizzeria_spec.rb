03ccd4ac822355a4dd9c151115a8a54e43135891
henrykerron <henrykerron@gmail.com>
2015-01-09 13:00:36 -0500

    Add reviews dependency for Pizzeria

--- spec/models/pizzeria_spec.rb
+++ spec/models/pizzeria_spec.rb
@@ -3,8 +3,9 @@ require "rails_helper"
 describe Pizzeria do
   let(:blank_values) { [nil, ""] }
 
-  describe "#pizzeria" do
+  describe "#name" do
     it { should have_valid(:name).when("#{'x' * 2 }", "#{'x' * 100 }") }
     it { should_not have_valid(:name).when("x", "#{'x' * 101 }", *blank_values) }
   end
+    it { should have_many(:reviews).dependent(:destroy)}
 end
